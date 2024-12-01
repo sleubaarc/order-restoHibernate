55b914c0c0d7501735f29756290f4557f4a8eead
sleubaarc <steeve.leuba@he-arc.ch>
2024-12-01 13:58:14 +0100

    Hbernate-annotated the OrganizationCustomer class

--- src/main/java/ch/hearc/ig/orderresto/business/OrganizationCustomer.java
+++ src/main/java/ch/hearc/ig/orderresto/business/OrganizationCustomer.java
@@ -1,9 +1,21 @@
 package ch.hearc.ig.orderresto.business;
 
-public class OrganizationCustomer extends Customer {
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 
+@Entity
+@DiscriminatorValue("O")
+public class OrganizationCustomer extends Customer {
+    @Column(name = "NOM")
     private String name;
+
+    @Column(name = "FORME_SOCIALE")
     private String legalForm;
+
+    public OrganizationCustomer() {
+        super();
+    }
 
     public OrganizationCustomer(Long id, String phone, String email, Address address, String name, String legalForm, String type) {
         super(id, phone, email, address, type);
