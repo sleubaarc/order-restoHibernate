3287b43a46d4783b235296bb5ba7cb76e4f987dc
sleubaarc <steeve.leuba@he-arc.ch>
2024-12-01 14:01:29 +0100

    Hbernate-annotated the PrivateCustomer class

--- src/main/java/ch/hearc/ig/orderresto/business/PrivateCustomer.java
+++ src/main/java/ch/hearc/ig/orderresto/business/PrivateCustomer.java
@@ -1,11 +1,24 @@
 package ch.hearc.ig.orderresto.business;
 
-public class PrivateCustomer extends Customer {
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 
+@Entity
+@DiscriminatorValue("P")
+public class PrivateCustomer extends Customer {
+    @Column(name = "EST_UNE_FEMME")
     private String gender;
+
+    @Column(name = "PRENOM")
     private String firstName;
+
+    @Column(name = "NOM")
     private String lastName;
 
+    public PrivateCustomer() {
+        super();
+    }
     public PrivateCustomer(Long id, String phone, String email, Address address, String gender, String firstName, String lastName, String type) {
         super(id, phone, email, address, type);
         this.gender = gender;
