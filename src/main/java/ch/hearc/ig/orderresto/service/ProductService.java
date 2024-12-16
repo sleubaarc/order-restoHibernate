2a4395005a06a72cc7d940fe593e1446eb2d446e
sleubaarc <steeve.leuba@he-arc.ch>
2024-12-16 20:40:21 +0100

    Update ProductService to use inTransaction Method

--- src/main/java/ch/hearc/ig/orderresto/service/ProductService.java
+++ src/main/java/ch/hearc/ig/orderresto/service/ProductService.java
@@ -9,24 +9,13 @@ import java.util.List;
 public class ProductService {
 
     public List<Product> getProductsByRestaurant(Restaurant restaurant) {
-        EntityManager em = null;
-        try {
-            // Obtenir l'EntityManager via JpaUtils
-            em = JpaUtils.getEntityManager();
-
-            // Créer une requête JPQL pour filtrer les produits par restaurant
+        return JpaUtils.inTransaction(em -> {
             TypedQuery<Product> query = em.createQuery(
-                    "SELECT p FROM Product p WHERE p.restaurant.id = :restaurantId", Product.class);
+                    "SELECT p FROM Product p " +
+                            "LEFT JOIN FETCH p.restaurant r " +
+                            "WHERE p.restaurant.id = :restaurantId", Product.class);
             query.setParameter("restaurantId", restaurant.getId());
-
-            // Exécuter la requête et retourner la liste
             return query.getResultList();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Erreur lors de la récupération des produits pour le restaurant " + restaurant.getName());
-            return null;
-        } finally {
-            JpaUtils.closeEntityManager();
-        }
+        });
     }
 }
